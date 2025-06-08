522078f1f8ef9477945d762a4148731c781e19d6
Davonne Vigil <davonnejv@gmail.com>
2025-06-07 21:58:38 -0500

    update reply section with styles

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -16,26 +16,42 @@ function FormComponent(props: {
   };
 
   return (
-    <div className="form-component">
-      <div className="current-user">
-        <div className="user-img">
-          <img
-            src={data.currentUser.image.png}
-            alt=""
-            className="w-10 rounded-2xl"
-          />
+    <div className="flex flex-col justify-center items-center w-full">
+      <div className="form-component max-w-3xl mt-4 mb-8 bg-blue-300 p-2 w-full">
+        <div className="flex gap-4 justify-center items-center">
+          <div className="current-user">
+            {/* <div className="user-img"> */}
+            {/* <img
+              src={data.currentUser.image.png}
+              alt=""
+              className="w-20 rounded-2xl"
+            /> */}
+            {/* </div> */}
+          </div>
+          <form
+            action="#"
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-2 justify-center items-center"
+          >
+            <h1 className="font-marker text-3xl">Leave a Comment:</h1>
+            <img
+              src={data.currentUser.image.png}
+              alt=""
+              className="w-20 rounded-2xl mx-auto"
+            />
+            <textarea
+              name="comment"
+              id="comment"
+              defaultValue={props.defaultValue}
+              placeholder="Add a comment..."
+              className="bg-white rounded text-2xl w-20 h-10 p-1 font-serif"
+            ></textarea>
+            <button className="font-marker ml-18 text-2xl w-20 hover:scale-105 transition-transform duration-300">
+              Send
+            </button>
+          </form>
         </div>
       </div>
-
-      <form action="#" onSubmit={handleSubmit}>
-        <textarea
-          name="comment"
-          id="comment"
-          defaultValue={props.defaultValue}
-          placeholder="Add a comment..."
-        ></textarea>
-        <button>Send</button>
-      </form>
     </div>
   );
 }
